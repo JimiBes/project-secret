f8ba71d0f002ef647b546997999dab31cc918d23
Marston <jimidbz@hotmail.fr>
2023-06-27 15:35:17 +0200

    modal created with a header and a footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <div className="footer">
+      <p>Copyright © 2023 Tous droits reservés.</p>
+      <p>Created by Jimi Bes</p>
+    </div>
+  );
+}
+
+export default Footer;
